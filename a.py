089354e9f87959eea596b211a8b72546acb98b63
mohammad.kabir <mohammad.kabir@uit.no>
2022-06-03 14:36:39 +0200

    a

--- a.py
+++ a.py
@@ -1,4 +1,11 @@
 print("Hello kk")
 print("From br2")
 print("from br1 l1")
+<<<<<<< HEAD
 print("from br1 l2")
+=======
+print("commit 1")
+print("commit 2")
+print("commit 4")
+print("commit 5")
+>>>>>>> commit 5
